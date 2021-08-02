50106dcf33ff79a9213a5e1e2ad0f2470738619a
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-08-02 14:06:43 +0530

    displayArrayInReverse added

---
+++ displayArrayReverse.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class displayArrayReverse {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc  = new Scanner(System.in);
+        //taking inputs
+        // n --> size of array
+        int n = sc.nextInt();
+        // define array of size n
+        int [] arr = new int[n];
+        //taking inputs in array
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        //calling display Array in reverse function
+        displayArrReverse(arr,0);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) {
+        //recursion break point
+        if(idx==arr.length){
+            return;
+        }
+        
+        //calling display array in reverse function with next index
+        displayArrReverse(arr,idx+1);
+        //printing element of array at index idx
+        System.out.println(arr[idx]);
+    }
+}
