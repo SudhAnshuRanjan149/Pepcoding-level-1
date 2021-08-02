f9de44f74a9d2d6a75f435e09c43b72d660b341b
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-08-02 12:38:27 +0530

    power logarithmic added

---
+++ powerLogarithmic.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class powerLogarithmic {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        int ans = power(x,n);
+        System.out.println(ans);
+    }
+
+    public static int power(int x, int n){
+        if(n==1){
+            return x;
+        }
+        int ans;
+        if(n%2==0){
+            int ans1 = power(x,n/2);
+            ans = ans1 * ans1;
+        }else{
+            int ans1 = power(x,n/2);
+            ans = x * ans1 * ans1;
+        }
+        
+        return ans;
+    }
+}
