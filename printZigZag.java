2a3131595ee2745f339b92e048a5a7303a7866e5
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-08-02 12:28:24 +0530

    printZigZag added

---
+++ printZigZag.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class printZigZag {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        sc.close();
+        pzz(n);
+        
+    }
+
+    public static void pzz(int n){
+        if(n==0){
+            return;
+        }
+        
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+        
+    
+    }
+
+}
