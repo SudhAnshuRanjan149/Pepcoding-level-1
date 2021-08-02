0594ac5975e7f979beb7f46e7210e66d4bb6007c
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-08-02 13:59:58 +0530

    displayArray added

--- displayArray.java
+++ displayArray.java
@@ -3,21 +3,29 @@ public class displayArray {
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner sc  = new Scanner(System.in);
+        //taking inputs
+        // n --> size of array
         int n = sc.nextInt();
+        // define array of size n
         int [] arr = new int[n];
+        //taking inputs in array
         for(int i=0;i<arr.length;i++){
             arr[i] = sc.nextInt();
         }
         sc.close();
+        //calling display Array function
         displayArr(arr,0);
     }
 
     public static void displayArr(int[] arr, int idx){
+        //recursion break point
         if(idx==arr.length){
             return;
         }
         
+        //printing element of array at index idx
         System.out.println(arr[idx]);
+        //calling display array function with next index
         displayArr(arr,idx+1);
     }
 
